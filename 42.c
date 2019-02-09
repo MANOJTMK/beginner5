2d450b630fad7713c5e3384302d787b159909b0a
MANOJTMK <46492877+MANOJTMK@users.noreply.github.com>
2019-02-09 15:44:53 +0530

    Update 42.c

--- 42.c
+++ 42.c
@@ -3,18 +3,18 @@
 int main(void) 
 {
 	char str1[50],str2[50];
-	int n1,n2;
+	int num1,num2;
 	printf("enter the string1:");
 	scanf("%s",str1);
 	printf("enter the string2:");
 	scanf("%s",str2);
-	n1=strlen(str1);
-	n2=strlen(str2);
-	if(n1>n2)
+	num1=strlen(str1);
+	num2=strlen(str2);
+	if(num1>num2)
 	{
 		printf("\n%s",str1);
 	}
-	else if(n2>n1)
+	else if(num2>num1)
 	{
 		printf("\n%s",str2);
 	}
